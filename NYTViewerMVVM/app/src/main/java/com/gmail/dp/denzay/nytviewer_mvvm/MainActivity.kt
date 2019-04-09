4af671ed64c7182f3f765dad1044ba7dc0227e4a
denzay_dp <denzay.dp@gmail.com>
2019-04-09 23:46:45 +0300

    Added Dagger2 dependencies

--- NYTViewerMVVM/app/src/main/java/com/gmail/dp/denzay/nytviewer_mvvm/MainActivity.kt
+++ NYTViewerMVVM/app/src/main/java/com/gmail/dp/denzay/nytviewer_mvvm/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.gmail.dp.denzay.nytviewer_mvvm_mvvm
+package com.gmail.dp.denzay.nytviewer_mvvm
 
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
