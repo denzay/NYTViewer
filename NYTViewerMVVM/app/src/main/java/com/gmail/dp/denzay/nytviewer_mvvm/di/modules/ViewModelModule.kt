2f6ebc4005184734e0bdf6ae7201228106b01bb1
denzay_dp <denzay.dp@gmail.com>
2019-04-15 22:44:03 +0300

    Inject view-models without input params

--- NYTViewerMVVM/app/src/main/java/com/gmail/dp/denzay/nytviewer_mvvm/di/modules/ViewModelModule.kt
+++ NYTViewerMVVM/app/src/main/java/com/gmail/dp/denzay/nytviewer_mvvm/di/modules/ViewModelModule.kt
@@ -4,6 +4,8 @@ import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import com.gmail.dp.denzay.nytviewer_mvvm.di.ViewModelKey
 import com.gmail.dp.denzay.nytviewer_mvvm.presentation.activity_main.news.most_emailed.MostEmailedViewModel
+import com.gmail.dp.denzay.nytviewer_mvvm.presentation.activity_main.news.most_shared.MostSharedViewModel
+import com.gmail.dp.denzay.nytviewer_mvvm.presentation.activity_main.news.most_viewed.MostViewedViewModel
 import com.gmail.dp.denzay.nytviewer_mvvm.presentation.common.ViewModelFactory
 import dagger.Binds
 import dagger.Module
@@ -17,15 +19,15 @@ interface ViewModelModule {
     @ViewModelKey(MostEmailedViewModel::class)
     fun bindMostEmailedViewModel(mostEmailedViewModel: MostEmailedViewModel): ViewModel
 
-//    @Binds
-//    @IntoMap
-//    @ViewModelKey(MostSharedViewModel::class)
-//    fun bindMostSharedViewModel(mostSharedViewModel: MostSharedViewModel): ViewModel
-//
-//    @Binds
-//    @IntoMap
-//    @ViewModelKey(MostViewedViewModel::class)
-//    fun bindMostViewedViewModel(mostViewedViewModel: MostViewedViewModel): ViewModel
+    @Binds
+    @IntoMap
+    @ViewModelKey(MostSharedViewModel::class)
+    fun bindMostSharedViewModel(mostSharedViewModel: MostSharedViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MostViewedViewModel::class)
+    fun bindMostViewedViewModel(mostViewedViewModel: MostViewedViewModel): ViewModel
 
     @Binds
     fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
--- NYTViewerMVVM/app/src/main/java/com/gmail/dp/denzay/nytviewer_mvvm/presentation/activity_main/news/most_shared/MostSharedViewModel.kt
+++ NYTViewerMVVM/app/src/main/java/com/gmail/dp/denzay/nytviewer_mvvm/presentation/activity_main/news/most_shared/MostSharedViewModel.kt
@@ -1,30 +1,27 @@
 package com.gmail.dp.denzay.nytviewer_mvvm.presentation.activity_main.news.most_shared
 
 import com.gmail.dp.denzay.nytviewer_mvvm.domain.most_shared.MostSharedUseCase
-import com.gmail.dp.denzay.nytviewer_mvvm.domain.news_item.NewsItem
 import com.gmail.dp.denzay.nytviewer_mvvm.presentation.activity_main.news.common.BaseNewsViewModel
-import io.reactivex.Single
-import io.reactivex.rxkotlin.subscribeBy
 import javax.inject.Inject
 
 class MostSharedViewModel @Inject constructor(
 //        private val router: MostSharedContract.Router,
-        private val mostSharedUseCase: MostSharedUseCase
+//        private val mostSharedUseCase: MostSharedUseCase
 ) : BaseNewsViewModel(), MostSharedContract.ViewModel {
 
     override fun doLoadData() {
 
-        disposables.add(mostSharedUseCase.getMostSharedList()
-                .flatMap {
-                    val list = mutableListOf<NewsItem>()
-                    it.map {
-                        val newsItem = NewsItem(it.id!!, it.url!!, it.title!!, it.shortDesc!!, "")
-                        list.add(newsItem)
-                    }
-                    return@flatMap Single.just(list)
-                }
-                .subscribeBy(onSuccess = {
-                    newsList.postValue(it)
-                }))
+//        disposables.add(mostSharedUseCase.getMostSharedList()
+//                .flatMap {
+//                    val list = mutableListOf<NewsItem>()
+//                    it.map {
+//                        val newsItem = NewsItem(it.id!!, it.url!!, it.title!!, it.shortDesc!!, "")
+//                        list.add(newsItem)
+//                    }
+//                    return@flatMap Single.just(list)
+//                }
+//                .subscribeBy(onSuccess = {
+//                    newsList.postValue(it)
+//                }))
     }
 }
